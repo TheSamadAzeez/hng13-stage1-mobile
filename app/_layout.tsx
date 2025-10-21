7c85abc1c5666776b78f2751f38f1ccb23c0071f
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-10-21 20:23:22 +0100

    Wrap layout in SafeAreaProvider for improved safe area handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
 
 import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar barStyle={'light-content'} />
       <Stack screenOptions={{ headerShown: false }} />
-    </>
+    </SafeAreaProvider>
   );
 }
